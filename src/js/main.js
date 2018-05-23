103b9cb2d8da014da897e7b98cc597559a6cfae3
Barrett Quan <barrett.quan@liferay.com>
2018-05-23 15:34:17 -0700

    cleaned errors across all files -added transitions for links and buttons -refactored css in header

--- src/js/main.js
+++ src/js/main.js
@@ -9,24 +9,13 @@ AUI().ready(
 	}
 );
 
-console.log("Hello! Welcome to NYXL's development home page");
-
 var addFullWidth = (elem) => {
-	
 	let helloWorldPortlet = document.querySelector(elem);
-	
-	if (!helloWorldPortlet) {
-		return console.log("did not add full width, check html of", elem)
-	} else {
-		helloWorldPortlet.classList.add("full-width");
-		return console.log("made full width", helloWorldPortlet);
+
+	if (helloWorldPortlet) {
+		helloWorldPortlet.classList.add('full-width');
 	}
-}
+	return helloWorldPortlet;
+};
 
-// var makeExcelsior = () => {
-// 	var bodyText = document.querySelector("#portlet_com_liferay_hello_world_web_portlet_HelloWorldPortlet").children[0].children[1].children[0].innerHTML
-// 	bodyText = "EXCELSIOR"
-// 	console.log('make xl', bodyText);
-// }	
-
-addEventListener("load", addFullWidth("#portlet_com_liferay_hello_world_web_portlet_HelloWorldPortlet"));
+addEventListener('load', addFullWidth('#portlet_com_liferay_hello_world_web_portlet_HelloWorldPortlet'));
