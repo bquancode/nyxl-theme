238b0c16f6f0bb9498168880170e4f8e9e7c640c
Barrett Quan <barrett.quan@liferay.com>
2018-05-25 09:59:47 -0700

    added z-index of child drop down element to fix mobile child page navigation -fixed table overflow issue on mobile

--- src/js/main.js
+++ src/js/main.js
@@ -19,14 +19,3 @@ var addFullWidth = (elem) => {
 };
 
 addEventListener('load', addFullWidth('#portlet_com_liferay_hello_world_web_portlet_HelloWorldPortlet'));
-
-var childElementNav = document.querySelector("#banner");
-
-addEventListener("click", function(event) {
-	if (event.target.className == ".icon-bar") {
-		//then make the child display to visable
-		console.log(event.target)
-	}
-	
-
-} )
